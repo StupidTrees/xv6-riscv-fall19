cdf333337bc11fd4aa68661b0e5525c2d55e53af
StupidTrees <2321182768@qq.com>
2020-11-11 15:07:13 +0800

    实验一

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,21 +2,21 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-    int fd1[2], fd2[2];
-    pipe(fd1);
-    pipe(fd2);
+    int parent_fd[2], child_fd[2];
+    pipe(parent_fd);
+    pipe(child_fd);
     char buf[64];
 
     if (fork()) {
         // 父进程
-        write(fd1[1], "ping", strlen("ping"));
-        read(fd2[0], buf, 4);
+        write(parent_fd[1], "ping", strlen("ping"));
+        read(child_fd[0], buf, 4);
         printf("%d: received %s\n", getpid(), buf);
     } else {
         // 子进程
-        read(fd1[0], buf, 4);
+        read(parent_fd[0], buf, 4);
         printf("%d: received %s\n", getpid(), buf);
-        write(fd2[1], "pong", strlen("pong"));
+        write(child_fd[1], "pong", strlen("pong"));
     }
 
     exit(0);
